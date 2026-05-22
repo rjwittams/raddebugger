6bab94d8979697fa621736689f9a351c95b176f8
Robert Wittams <robert@wittams.com>
2026-05-22 15:26:11 +0100

    mac: add IPC call stack dump

--- src/raddbg/raddbg_core.c
+++ src/raddbg/raddbg_core.c
@@ -12901,6 +12901,96 @@ rd_frame(void)
                 }
               }
             }
+            else if(str8_match(cmd_name, str8_lit("dump_call_stack"), 0))
+            {
+              handled = 1;
+              String8 thread_or_process_string = {0};
+              if(msg_parts.first != 0 && msg_parts.first->next != 0)
+              {
+                thread_or_process_string = msg_parts.first->next->string;
+              }
+
+              D_Entity *thread = &d_entity_nil;
+              if(thread_or_process_string.size != 0)
+              {
+                D_Entity *entity = d_entity_from_handle(d_handle_from_string(thread_or_process_string));
+                if(entity != &d_entity_nil)
+                {
+                  if(entity->kind == D_EntityKind_Process)
+                  {
+                    thread = d_entity_child_from_kind(entity, D_EntityKind_Thread);
+                  }
+                  else
+                  {
+                    thread = entity;
+                  }
+                }
+                else
+                {
+                  U64 pid = 0;
+                  if(try_u64_from_str8_c_rules(thread_or_process_string, &pid))
+                  {
+                    D_EntityArray processes = d_entity_array_from_kind(D_EntityKind_Process);
+                    for(U64 idx = 0; idx < processes.count; idx += 1)
+                    {
+                      D_Entity *process = processes.v[idx];
+                      if(process->id == pid)
+                      {
+                        thread = d_entity_child_from_kind(process, D_EntityKind_Thread);
+                        break;
+                      }
+                    }
+                  }
+                }
+              }
+              if(thread == &d_entity_nil)
+              {
+                thread = d_entity_from_handle(rd_base_regs()->thread);
+              }
+              if(thread == &d_entity_nil || thread->kind != D_EntityKind_Thread)
+              {
+                str8_list_pushf(rd_state->cmd_output_arena, &rd_state->cmd_outputs, "error: thread not found");
+              }
+              else
+              {
+                Access *access = access_open();
+                D_Entity *process = d_entity_ancestor_from_kind(thread, D_EntityKind_Process);
+                ARCH_Info *arch_info = arch_info_from_arch(thread->arch);
+                D_CallStack call_stack = d_call_stack_from_thread(access, thread->handle, 1, now_time_us()+1000000);
+                String8List lines = {0};
+                str8_list_pushf(scratch.arena, &lines, "frames:%I64u concrete:%I64u", call_stack.frames_count, call_stack.concrete_frames_count);
+                for(U64 idx = 0; idx < call_stack.frames_count; idx += 1)
+                {
+                  D_CallStackFrame *frame = &call_stack.frames[call_stack.frames_count - 1 - idx];
+                  U64 rip_vaddr = arch_ip_from_reg_block(arch_info, frame->regs);
+                  D_Entity *module = d_module_from_process_vaddr(process, rip_vaddr);
+                  String8 module_name = str8_lit("???");
+                  String8 procedure_name = str8_lit("???");
+                  if(module != &d_entity_nil)
+                  {
+                    module_name = str8_skip_last_slash(module->string);
+                    U64 rip_voff = d_voff_from_vaddr(module, rip_vaddr);
+                    DI_Key dbgi_key = d_dbgi_key_from_module(module);
+                    RDI_Parsed *rdi = di_rdi_from_key(access, dbgi_key, 0, 0);
+                    if(rdi != &rdi_parsed_nil)
+                    {
+                      RDI_Symbol *procedure = rdi_procedure_from_voff(rdi, rip_voff);
+                      String8 name = {0};
+                      name.str = rdi_string_from_idx(rdi, procedure->name_string_idx, &name.size);
+                      if(name.size != 0)
+                      {
+                        procedure_name = name;
+                      }
+                    }
+                  }
+                  str8_list_pushf(scratch.arena, &lines, "\n#%I64u 0x%I64x %S!%S cfa=0x%I64x unwind=%I64u inline=%I64u",
+                                  idx, rip_vaddr, module_name, procedure_name, frame->cfa, frame->unwind_count, frame->inline_depth);
+                }
+                String8 output = str8_list_join(rd_state->cmd_output_arena, &lines, 0);
+                str8_list_push(rd_state->cmd_output_arena, &rd_state->cmd_outputs, output);
+                access_close(access);
+              }
+            }
             if(!handled)
             {
             String8List msg_cmd_line_parts = {0};
