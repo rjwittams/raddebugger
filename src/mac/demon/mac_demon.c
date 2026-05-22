8368b45c80b44d1122ccd104d400c84bd263f016
Robert Wittams <robert@wittams.com>
2026-05-22 14:42:00 +0100

    mac: install exception ports for launched targets

--- src/mac/demon/mac_demon.c
+++ src/mac/demon/mac_demon.c
@@ -1862,8 +1862,19 @@ dmn_ctrl_launch(DMN_CtrlCtx *ctx, ProcessLaunchParams *params)
     kern_return_t task_result = task_for_pid(mach_task_self(), (int)pid, &task);
     if(task_result == KERN_SUCCESS && task != MACH_PORT_NULL)
     {
-      mac_dmn_process_entity_alloc(pid, task, 1, 1);
-      result = (U32)pid;
+      MAC_DMN_Entity *entity = mac_dmn_process_entity_alloc(pid, task, 1, 1);
+      MAC_DMN_Process *process = &entity->process;
+      if(mac_dmn_process_begin_mach_exceptions(process))
+      {
+        result = (U32)pid;
+      }
+      else
+      {
+        String8 exe = params->cmd_line.first ? params->cmd_line.first->string : str8_zero();
+        log_user_errorf("Could not launch `%S`: failed to install Mach exception port for pid %u.", exe, (U32)pid);
+        mac_dmn_kill_launched_child(pid);
+        mac_dmn_process_entity_release(entity);
+      }
     }
     else
     {
