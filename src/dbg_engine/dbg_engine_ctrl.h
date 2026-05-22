04f843f88d17f151670513ba57fdf4651d6c09d5
Robert Wittams <robert@wittams.com>
2026-05-22 11:17:56 +0100

    mac: support x64 stack-indirect compact unwind

--- src/dbg_engine/dbg_engine_ctrl.h
+++ src/dbg_engine/dbg_engine_ctrl.h
@@ -560,7 +560,8 @@ internal String8 d_macho_unwind_info_data_from_module(Arena *arena, D_Handle mod
 internal String8 d_eh_frame_hdr_from_eh_frame(Arena *arena, String8 eh_frame_data, U64 eh_frame_vaddr, Arch arch, EH_PtrCtx *eh_ptr_ctx);
 internal U64 *d_unwind_reg_from_macho_reg__macho_x64(X64_RegBlock *regs, MachO_UnwindX64Reg macho_reg);
 internal B32 d_macho_compact_unwind_x64_mode_is_supported(U32 encoding);
-internal B32 d_macho_compact_unwind_x64_cfa_from_encoding(U32 encoding, X64_RegBlock *regs, U64 *cfa_out);
+internal B32 d_macho_compact_unwind_x64_frameless_stack_size_from_encoding(D_Handle process_handle, U64 module_base_vaddr, MachO_UnwindInfoLookupResult *lookup, B32 *is_stale_out, U64 endt_us, U64 *stack_size_out);
+internal B32 d_macho_compact_unwind_x64_cfa_from_encoding(D_Handle process_handle, U64 module_base_vaddr, MachO_UnwindInfoLookupResult *lookup, X64_RegBlock *regs, B32 *is_stale_out, U64 endt_us, U64 *cfa_out);
 internal D_UnwindStepResult d_unwind_step__macho_x64(D_Handle process_handle, D_Handle module_handle, U64 module_base_vaddr, X64_RegBlock *regs, U64 endt_us);
 internal D_Unwind d_unwind_from_thread(Arena *arena, D_Handle thread, U64 endt_us);
 
