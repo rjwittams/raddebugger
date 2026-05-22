01809a10cbaae973cee6ed007fd42757d0523c4a
Robert Wittams <robert@wittams.com>
2026-05-22 19:50:04 +0100

    mac: improve Darwin arm64 stepping footing

--- src/dbg_engine/dbg_engine_user.c
+++ src/dbg_engine/dbg_engine_user.c
@@ -2013,6 +2013,23 @@ d_tick(Arena *arena, D_TargetArray *targets, D_BreakpointArray *breakpoints, D_P
           D_Entity *thread = d_entity_from_handle(params->thread);
           if(thread == &d_entity_nil)
           {
+            thread = d_entity_from_handle(d_user_state->ctrl_last_stop_event.entity);
+          }
+          if(thread == &d_entity_nil)
+          {
+            D_EntityArray threads = d_entity_array_from_kind(D_EntityKind_Thread);
+            for EachIndex(idx, threads.count)
+            {
+              D_Entity *candidate = threads.v[idx];
+              if(!candidate->is_frozen)
+              {
+                thread = candidate;
+                break;
+              }
+            }
+          }
+          if(thread == &d_entity_nil)
+          {
             log_user_error(str8_lit("Must have a selected thread to step."));
           }
           else if(d_ctrl_targets_running())
