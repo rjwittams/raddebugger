5501928c79a403b374351e188a950afbe53c121d
Robert Wittams <robert@wittams.com>
2026-05-23 22:38:52 +0100

    hygiene: clean arm64 footing whitespace

--- src/torture/torture_arch.c
+++ src/torture/torture_arch.c
@@ -19,13 +19,13 @@ TEST(arm64_metadata)
   T_Ok(max_ops_per_instruction_from_arch(Arch_arm64) == 1);
   T_Ok(min_instruction_size_from_arch(Arch_arm64) == 4);
   T_Ok(max_instruction_size_from_arch(Arch_arm64) == 4);
-  
+
   ARM64_RegBlock regs = {0};
   regs.pc = 0x1234567887654321ull;
   regs.sp = 0x100000000ull;
   T_Ok(arch_ip_from_reg_block(info, &regs) == regs.pc);
   T_Ok(arch_sp_from_reg_block(info, &regs) == regs.sp);
-  
+
   U64 new_ip = 0x200000000ull;
   U64 new_sp = 0x300000000ull;
   T_Ok(arch_reg_block_write_ip(info, &regs, new_ip));
@@ -56,12 +56,12 @@ TEST(arm64_rdi_dwarf_mappings)
 {
   T_Ok(rdi_arch_from_arch(Arch_arm64) == RDI_Arch_ARM64);
   T_Ok(arch_from_rdi_arch(RDI_Arch_ARM64) == Arch_arm64);
-  
+
   ARCH_Info *info = arch_info_from_arch(Arch_arm64);
   T_Ok(arch_reg_code_from_name(info, str8_lit("pc")) == ARM64_RegCode_pc);
   T_Ok(arch_reg_code_from_rdi(Arch_arm64, RDI_RegCodeARM64_pc) == ARM64_RegCode_pc);
   T_Ok(arch_reg_code_from_dw(Arch_arm64, DW_RegARM64_Pc) == ARM64_RegCode_pc);
-  
+
   T_Ok(dw_reg_size_from_code(Arch_arm64, DW_RegARM64_X0) == 8);
   T_Ok(dw_reg_size_from_code(Arch_arm64, DW_RegARM64_Pc) == 8);
   T_Ok(dw_reg_pos_from_code(Arch_arm64, DW_RegARM64_Pc) == 0);
