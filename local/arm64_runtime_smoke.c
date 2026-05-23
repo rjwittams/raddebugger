a4926f59ea308859d27e6c16636eae37eb9bef98
Robert Wittams <robert@wittams.com>
2026-05-23 07:44:03 +0100

    mac: add ARM64 runtime smoke hooks

---
+++ local/arm64_runtime_smoke.c
@@ -0,0 +1,52 @@
+#include <stdint.h>
+
+volatile uint64_t watch_value = 0;
+
+__attribute__((noinline)) void
+smoke_vector_stop(void)
+{
+  __asm__ volatile(
+    "movi v0.16b, #0x11\n"
+    "movi v8.16b, #0x88\n"
+    "movi v31.16b, #0x31\n"
+    "brk #0x1234\n"
+    :
+    :
+    : "v0", "v8", "v31");
+}
+
+__attribute__((noinline)) void
+smoke_watch_write(void)
+{
+  watch_value = 0x1122334455667788ull;
+}
+
+__attribute__((noinline)) uint64_t
+smoke_unwind_c(uint64_t x)
+{
+  volatile uint64_t y = x + 3;
+  __asm__ volatile("brk #0x1236");
+  return y;
+}
+
+__attribute__((noinline)) uint64_t
+smoke_unwind_b(uint64_t x)
+{
+  volatile uint64_t y = x + 2;
+  return smoke_unwind_c(y) + y;
+}
+
+__attribute__((noinline)) uint64_t
+smoke_unwind_a(uint64_t x)
+{
+  volatile uint64_t y = x + 1;
+  return smoke_unwind_b(y) + y;
+}
+
+int
+main(void)
+{
+  smoke_vector_stop();
+  smoke_watch_write();
+  return (int)smoke_unwind_a(watch_value);
+}
