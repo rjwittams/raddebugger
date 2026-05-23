55efea18480a59c275ae371c6e6c8e5eedf5dc8f
Robert Wittams <robert@wittams.com>
2026-05-23 23:50:32 +0100

    mac: strengthen arm64 unwind fallbacks

--- src/dbg_engine/dbg_engine_ctrl.h
+++ src/dbg_engine/dbg_engine_ctrl.h
@@ -597,7 +597,11 @@ internal B32 d_macho_compact_unwind_arm64_mode_is_supported(U32 encoding);
 internal U64 d_macho_compact_unwind_arm64_frame_saved_reg_slots_from_encoding(U32 encoding, D_MachOCompactArm64FrameRegSlot *slots, U64 slots_cap);
 internal B32 d_macho_compact_unwind_arm64_write_reg_slot(ARM64_RegBlock *regs, D_MachOCompactArm64FrameRegSlot *slot, U64 value);
 internal B32 d_macho_compact_unwind_arm64_cfa_from_encoding(MachO_UnwindInfoLookupResult *lookup, ARM64_RegBlock *regs, U64 *cfa_out);
-internal D_UnwindStepResult d_unwind_step__macho_arm64(D_Handle process_handle, D_Handle module_handle, U64 module_base_vaddr, ARM64_RegBlock *regs, U64 endt_us);
+internal B32 d_unwind_arm64_cfa_from_frame_pointer(ARM64_RegBlock *regs, U64 *cfa_out);
+internal B32 d_unwind_arm64_cfa_from_link_register(ARM64_RegBlock *regs, U64 *cfa_out);
+internal D_UnwindStepResult d_unwind_step__link_register_arm64(ARM64_RegBlock *regs);
+internal D_UnwindStepResult d_unwind_step__frame_pointer_arm64(D_Handle process_handle, ARM64_RegBlock *regs, U64 endt_us);
+internal D_UnwindStepResult d_unwind_step__macho_arm64(D_Handle process_handle, D_Handle module_handle, U64 module_base_vaddr, ARM64_RegBlock *regs, B32 prefer_entry_lr, U64 endt_us);
 
 //- rjf: abstracted full unwind
 internal D_Unwind d_unwind_from_thread(Arena *arena, D_Handle thread, U64 endt_us);
