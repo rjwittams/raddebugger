3253a353befa16844e611f448ac0c34073dfb5eb
Robert Wittams <robert@wittams.com>
2026-05-23 08:19:59 +0100

    mac: tighten ARM64 runtime smoke

--- local/arm64_runtime_smoke.c
+++ local/arm64_runtime_smoke.c
@@ -3,16 +3,22 @@
 volatile uint64_t watch_value = 0;
 
 __attribute__((noinline)) void
+smoke_vector_ready(void)
+{
+  __asm__ volatile("" ::: "memory");
+}
+
+__attribute__((noinline)) void
 smoke_vector_stop(void)
 {
   __asm__ volatile(
     "movi v0.16b, #0x11\n"
     "movi v8.16b, #0x88\n"
     "movi v31.16b, #0x31\n"
-    "brk #0x1234\n"
     :
     :
     : "v0", "v8", "v31");
+  smoke_vector_ready();
 }
 
 __attribute__((noinline)) void
@@ -25,7 +31,6 @@ __attribute__((noinline)) uint64_t
 smoke_unwind_c(uint64_t x)
 {
   volatile uint64_t y = x + 3;
-  __asm__ volatile("brk #0x1236");
   return y;
 }
 
