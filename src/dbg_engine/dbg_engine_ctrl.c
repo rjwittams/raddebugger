bb87ef3f00b4cbffa5eb2e3ee3eea5fb04f99863
Robert Wittams <robert@wittams.com>
2026-05-27 17:26:08 +0100

    Read Mach-O function starts from mapped images

--- src/dbg_engine/dbg_engine_ctrl.c
+++ src/dbg_engine/dbg_engine_ctrl.c
@@ -3919,6 +3919,8 @@ d_ctrl_thread__next_dmn_event(Arena *arena, DMN_CtrlCtx *ctrl_ctx, D_Msg *msg, D
               U64 text_vaddr = 0;
               Rng1U64 eh_frame_vrange = {0};
               Rng1U64 unwind_info_vrange = {0};
+              Rng1U64 function_starts_vrange = {0};
+              macho_linkedit_data_vrange_from_bin(data, &bin, MACHO_LC_FUNCTION_STARTS, slide, &function_starts_vrange);
               module_info->arch = arch;
               String8 dsym_path = macho_dsym_path_from_executable_path(scratch2.arena, module_path);
               FileProperties dsym_props = properties_from_file_path(dsym_path);
@@ -3988,6 +3990,14 @@ d_ctrl_thread__next_dmn_event(Arena *arena, DMN_CtrlCtx *ctrl_ctx, D_Msg *msg, D
               {
                 macho_unwind_info_data = d_data_from_process_vaddr_range(arena, process_handle, unwind_info_vrange, 0);
               }
+              if(function_starts_vrange.max > function_starts_vrange.min)
+              {
+                String8 function_starts_data = d_data_from_process_vaddr_range(arena, process_handle, function_starts_vrange, 0);
+                if(function_starts_data.size == dim_1u64(function_starts_vrange))
+                {
+                  macho_function_start_voffs = macho_function_start_voffs_from_base_vaddr_data(arena, file_base_vaddr, text_vaddr - slide, function_starts_data);
+                }
+              }
               if(eh_frame_vrange.max > eh_frame_vrange.min)
               {
                 String8 eh_frame_data = d_data_from_process_vaddr_range(arena, process_handle, eh_frame_vrange, 0);
@@ -4012,7 +4022,10 @@ d_ctrl_thread__next_dmn_event(Arena *arena, DMN_CtrlCtx *ctrl_ctx, D_Msg *msg, D
               if(file_data.size != 0)
               {
                 MachO_Bin file_bin = macho_bin_from_data(scratch2.arena, file_data);
-                macho_function_start_voffs = macho_function_start_voffs_from_data(arena, file_data, &file_bin);
+                if(macho_function_start_voffs.count == 0)
+                {
+                  macho_function_start_voffs = macho_function_start_voffs_from_data(arena, file_data, &file_bin);
+                }
               }
             }
           }
