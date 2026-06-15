1e1837a00e9ab360f117c9def5e8d241d838cb9d
Robert Wittams <robert@wittams.com>
2026-06-15 03:03:07 +0100

    arch: add portable arm64 architecture footing

--- src/raddbg/raddbg_main.c
+++ src/raddbg/raddbg_main.c
@@ -368,6 +368,7 @@
 //- rjf: [h]
 #include "base/base_inc.h"
 #include "x64/x64.h"
+#include "arm64/arm64.h"
 #include "win32/win32_inc.h"
 #include "linker/hash_table.h"
 #include "linker/base_ext/base_bit_array.h"
@@ -428,6 +429,7 @@
 //- rjf: [c]
 #include "base/base_inc.c"
 #include "x64/x64.c"
+#include "arm64/arm64.c"
 #include "win32/win32_inc.c"
 #include "linker/hash_table.c"
 #include "linker/base_ext/base_bit_array.c"
--- src/torture/torture_main.c
+++ src/torture/torture_main.c
@@ -22,6 +22,7 @@
 
 #include "base/base_inc.h"
 #include "x64/x64.h"
+#include "arm64/arm64.h"
 #include "linker/hash_table.h"
 #include "linker/lf_hash_table.h"
 #include "linker/base_ext/base_bit_array.h"
@@ -102,6 +103,7 @@
 
 #include "base/base_inc.c"
 #include "x64/x64.c"
+#include "arm64/arm64.c"
 #include "linker/hash_table.c"
 #include "linker/lf_hash_table.c"
 #include "linker/base_ext/base_bit_array.c"
@@ -176,6 +178,7 @@
 #include "linker/lnk_debug_helper.c"
 #include "torture.c"
 
+#include "torture_arch.c"
 #include "base/tests/base_tests.c"
 #include "mdesk/tests/mdesk_tests.c"
 #include "linker/tests/linker_tests.c"
@@ -193,4 +196,3 @@ entry_point(CmdLine *cmdline)
 {
   t_entry_point(cmdline);
 }
-
